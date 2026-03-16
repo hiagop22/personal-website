443fe0d79ecd506fa934395f85f6de746ce02039
Hiago dos Santos <hiagop22@gmail.com>
2026-03-16 19:34:23 -0300

    test: validate ci final status (#31)

--- infrastructure/s3/main.tf
+++ infrastructure/s3/main.tf
@@ -30,7 +30,8 @@ resource "aws_s3_object" "placeholder_index" {
     ignore_changes = [
       content,
       source,
-      etag
+      etag,
+      tags
     ]
   }
 }
@@ -42,31 +43,6 @@ resource "aws_s3_bucket_public_access_block" "website" {
   block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
-}
-
-# TERRAFORM DON`T WAIT THE NULL RESOURCE TO TRY UPLOADDING FILES. 
-# SO BUILD MANUALLY, OR IT WILL CRASH ALL PROCESS
-# resource "terraform_data" "angular_build" {
-#   triggers_replace = {
-#     layer_build = md5(join("-", [for x in fileset("${var.web_root_path}/src", local.md5_files_str) : filemd5(x)]))
-#   }
-
-#   provisioner "local-exec" {
-#     working_dir = "${var.web_root_path}"
-#     command = "ng build  --base-href 'https://${var.domain}'"
-#   }
-# }
-
-resource "aws_s3_object" "upload_object" {
-  for_each = fileset("${var.web_build_path}/", "**")
-
-  bucket       = aws_s3_bucket.website.id
-  key          = each.value
-  source       = "${var.web_build_path}/${each.value}"
-  etag         = filemd5("${var.web_build_path}/${each.value}")
-  content_type = lookup(var.file_types, regex("\\.[^\\.]+\\z", "${var.web_build_path}/${each.value}"), var.default_file_type)
-
-  # depends_on = [ terraform_data.angular_build ]
 }
 
 
